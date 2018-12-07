03d527ae8eb6c524ab50fe1a280411297089dc3c
Jacob Ellis <jacob@goellis.com>
2018-12-07 22:14:30 +0000

    day5part1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,68 +1,33 @@
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.text.SimpleDateFormat
-import kotlin.math.abs
-
-//[1518-11-05 00:03] Guard #99 begins shift
-//[1518-11-05 00:45] falls asleep
-//[1518-11-05 00:55] wakes up
-val regex = """\[(.*)] (.*)""".toRegex()
-val regexGuard = """Guard #(\d+) begins shift""".toRegex()
-val dateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm")
-
-inline class Minute(val minute: Int)
-inline class Guard(val guard: Int)
 
 fun main(args: Array<String>) {
-    val allLines = Files.readAllLines(Paths.get("/home/jacob/dev/advent/src/main/kotlin/Data"))
+    val chars = Files.readAllLines(Paths.get("/home/jacob/dev/advent/src/main/kotlin/Data")).first().toMutableList()
 
-    val map = mutableMapOf<Guard, MutableMap<Minute, Int>>()
-    var lastMatch: MatchResult? = null
-
-    val sortedBy = allLines
-        .map {
-            val vals = regex.find(it)!!.groupValues
-            dateFormat.parse(vals[1]) to vals[2]
-        }
-        .sortedBy { it.first }
-
-    println(sortedBy.map { it.first to it.second + '\n' })
-
-    val iterator = sortedBy
-        .iterator()
-
-
-    var guard = Guard(-1)
-    while (iterator.hasNext()) {
-        val it = iterator.next()
-
-        val guardMatch = regexGuard.find(it.second)
-        if (guardMatch != null) {
-            guard = Guard(guardMatch.groupValues[1].toInt())
-            continue
+    while (true) {
+        var last = '0'
+        var reacted = false
+        for (i in 0 until chars.size) {
+            if (chars[i].reactsWith(last)) {
+                chars.removeAt(i)
+                chars.removeAt(i - 1)
+                reacted = true
+                break
+            }
+            last = chars[i]
         }
 
-        val start = Minute(it.first.minutes)
-        val end = Minute(iterator.next().first.minutes)
-
-        val guardMins = map.getOrPut(guard) { mutableMapOf() }
-
-        for (min in start.minute until end.minute) {
-            guardMins[Minute(min)] = guardMins.getOrDefault(Minute(min), 0) + 1
-        }
+        if (!reacted) break
     }
 
-    val (mostGuard, value) = map.entries.flatMap { entry -> entry.value.map { entry.key to it } }.maxBy { it.second.value }!!
-
-    value.key.minute
-
-    map.entries.forEach {
-        println(it)
-        println(it.value.map { it.value }.sum())
-    }
+    val ret = chars.fold("") { acc, c -> acc + c }
 
     println("fin")
-    println("${value.key.minute}")
-    println("${mostGuard.guard}")
-    println("${value.key.minute * mostGuard.guard}")
+    println(ret.length)
+}
+
+private fun Char.reactsWith(last: Char) = if (isUpperCase()) {
+    last.isLowerCase() && equals(last.toUpperCase())
+} else {
+    last.isUpperCase() && equals(last.toLowerCase())
 }
