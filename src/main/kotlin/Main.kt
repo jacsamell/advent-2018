09568aa85a5add2350e8eaf218b90c36051fcca0
Jacob Ellis <jacob@goellis.com>
2018-12-22 17:05:43 +0000

    day12part2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,63 +1,62 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import java.util.stream.Collectors
+import java.nio.file.Files
+import java.nio.file.Paths
 
-val serial = 3031
+val regex = """([.#]+) => ([.#])""".toRegex()
 
 fun main(args: Array<String>) {
-    val (size, max) = IntRange(1, 300)
-        .toList().parallelStream()
-        .map {
-            println("Start $it")
-            val pair = it to maxPower(it)
-            println("Done $it")
-            pair
-        }.collect(Collectors.toList())
-        .maxBy { it.second.value.maxBy { it.value }!!.value }!!
+    var start = 0L
+    val allLines = Files.readAllLines(Paths.get("/home/jacob/dev/advent/src/main/kotlin/Data"))
 
-    //println(cells.map { it.map { it.absoluteValue } + "\n" })
-    println(max.index + 1)
-    val maxBy = max.value.maxBy { it.value }!!
-    println(maxBy.index + 1)
-    println(size)
+    val initial = allLines.removeAt(0).removePrefix("initial state: ")
+    allLines.removeAt(0)
 
-
-    println()
-    println(maxBy.value)
-}
-
-private fun maxPower(size: Int): IndexedValue<Iterable<IndexedValue<Int>>> {
-    val cells = Array(300) { i ->
-        IntArray(300) { j ->
-            val x = i + 1
-            val y = j + 1
-            val rack = x + 10
-            val digits = (rack * y + serial) * rack
-            val string = digits.toString()
-            string.getOrElse(string.length - 3) { '0' }.toString().toInt() - 5
-        }
+    val rules = allLines.map {
+        val groupValues = regex.find(it)!!.groupValues
+        val rule = groupValues[1].replace(".", "\\.")
+        """(?=($rule)).""".toRegex() to groupValues[2]
     }
 
-    val max = cells.mapIndexed { i, a ->
-        a.mapIndexed { j, it ->
-            try {
-                sumAll(cells, i, j, size)
-            } catch (e: ArrayIndexOutOfBoundsException) {
-                0
+    var current = initial
+    for (i in 1..50000000000) {
+        while (!current.startsWith("....")) {
+            start--
+            current = ".$current"
+        }
+        while (current.startsWith(".....")) {
+            start++
+            current = current.substring(1)
+        }
+        while (!current.endsWith("....")) {
+            current = "$current."
+        }
+
+        var next = current.replace('#', '.')
+
+        rules.forEach {
+            it.first.findAll(current).forEach { match ->
+                val index = match.range.first + 2
+                next = next.replaceRange(index, index + 1, it.second)
             }
-        }.withIndex()
-    }.withIndex().maxBy { it.value.maxBy { it1 -> it1.value }!!.value }!!
-    return max
-}
+        }
 
-private fun sumAll(cells: Array<IntArray>, i: Int, j: Int, size: Int): Int {
-    var ret = 0
+        println(i)
+        println(start)
+        println(next)
 
-    for (x in i until i + size) {
-        for (y in j until j + size) {
-            ret += cells[x][y]
+        if (i>10000) {
+            start+=50000000001-i
+            break
+        }
+
+        current = next
+    }
+
+    var score = 0L
+    current.forEachIndexed { index, c ->
+        if (c == '#') {
+            score += index + start
         }
     }
 
-    return ret
+    println(score)
 }
