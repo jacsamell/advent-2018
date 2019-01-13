fcf69604ea06155cb0013d25383be37d16c8a950
Jacob Ellis <jacob@goellis.com>
2019-01-13 18:48:35 +0000

    day18part2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,6 @@
+import java.lang.RuntimeException
 import java.nio.file.Files
 import java.nio.file.Paths
-
-// y=1595, x=454..478
-
-val regexX = """x=(\d+(?:\.\.\d+)?)""".toRegex()
-val regexY = """y=(\d+(?:\.\.\d+)?)""".toRegex()
 
 fun main(args: Array<String>) {
     Main().main()
@@ -12,144 +8,107 @@ fun main(args: Array<String>) {
 
 class Main {
     private val lines = Files.readAllLines(Paths.get("/home/jacob/dev/advent/src/main/kotlin/Data"))
+    var grid = lines.map { it.toCharArray() }.toTypedArray()
+    val size = grid.size
+    var newGrid = Array(size) { CharArray(size) }
 
-    private val grid = lines.flatMap {
-        val x = regexX.find(it)!!.groupValues[1]
-        val y = regexY.find(it)!!.groupValues[1]
-
-        if (x.contains("..")) {
-            val range = x.split("..").map { it.toInt() }
-            return@flatMap IntRange(range[0], range[1]).map { it to y.toInt() }
-        } else {
-            val range = y.split("..").map { it.toInt() }
-            return@flatMap IntRange(range[0], range[1]).map { x.toInt() to it }
-        }
-    }.map { it to "#" }.toMap().toMutableMap()
-
-    private val minY = grid.keys.minBy { it.second }!!.second
-    private val maxY = grid.keys.maxBy { it.second }!!.second
-
-    val water = mutableListOf<Pair<Int, Int>>()
-
-    private var counter = 0
+    val all = mutableListOf(grid.map { it.clone() }.toTypedArray())
 
     fun main() {
         print()
-        while (!calculateNextPath()) {
-        }
-        print()
-    }
+        var time = System.currentTimeMillis()
+        for (z in 0 until 1000000000) {
+            if (z % 10000 == 0) {
+                println("$z / 1000000000")
+                println(System.currentTimeMillis() - time)
+                time = System.currentTimeMillis()
 
-    private fun calculateNextPath(): Boolean {
+                print()
+            }
 
-        val oldWater = water.toMutableList().apply { add(500 to minY-1) }
-        water.clear()
-
-        val oldGrid = grid.toMap()
-
-        oldWater.forEach {
-            val x = it.first
-            val y = it.second
-
-            grid.remove(x to y)
-            //print(minY, maxY, minX, maxX, grid)
-
-            if (grid.blocked(x to y + 1)) {
-                if (y < maxY) {
-                    water.add(x to y + 1)
-                    grid[x to y + 1] = "|"
+            for (y in 0 until grid.size) {
+                val row = grid[y]
+                for (x in 0 until row.size) {
+                    val char = row[x]
+                    val new = when (char) {
+                        '.' -> if (adjContains('|', 3, x, y)) '|' else '.'
+                        '|' -> if (adjContains('#', 3, x, y)) '#' else '|'
+                        '#' -> if (adjContains('#', 1, x, y) && adjContains('|', 1, x, y)) '#' else '.'
+                        else -> throw RuntimeException()
+                    }
+                    newGrid[y][x] = new
                 }
-            } else if (!grid.contains(x + 1 to y) || !grid.contains(x - 1 to y)) {
-                if (!grid.contains(x + 1 to y)) {
-                    water.add(x + 1 to y)
-                    grid[x + 1 to y] = "|"
-                }
-                if (!grid.contains(x - 1 to y)) {
-                    water.add(x - 1 to y)
-                    grid[x - 1 to y] = "|"
-                }
-            } else if (cannotMove(grid, x, y, oldWater)) {
-                grid.put(x to y, "~")
+            }
+
+            val oldGrid = grid
+            grid = newGrid
+            newGrid = oldGrid
+
+           // print()
+
+            if (all.any { grid.contentDeepEquals(it) }) {
+
+                val (index, match) = all.withIndex().find { grid.contentDeepEquals(it.value) }!!
+                //
+                println()
+                println()
+
+                println(index)
+                println(all.size)
+                println(all.size - index)
+break
+                //all.
             } else {
-                water.add(x to y)
-                grid.put(x to y, "|")
+                all.add(grid.map { it.clone() }.toTypedArray())
             }
+            println(all.size)
 
-            //print(minY, maxY, minX, maxX, grid)
+            //print()
         }
 
-        //print()
+        val count = ((1000000000-511) % 28)+511
 
-        //val current = grid.filterValues { it == "|" || it == "~" }.keys.maxBy { it.second }!!.second
-        //println("$current / $maxY")
-        println("${++counter} of 13635")
+            val wood = all[count].flatMap { it.asList() }.filter { it == '|' }.size
+            val lumber = all[count].flatMap { it.asList() }.filter { it == '#' }.size
+            //print()
+            println("$wood $lumber")
+        println(wood * lumber)
 
-        if (oldGrid == grid) {
-            println(grid.filterValues { it == "|" || it == "~" }.size)
-            println(grid.filterValues { it == "~" }.size)
-            return true
-        }
-        return false
-    }
-
-    private fun cannotMove(
-        grid: MutableMap<Pair<Int, Int>, String>,
-        x: Int,
-        y: Int,
-        oldWater: MutableList<Pair<Int, Int>>
-    ): Boolean {
-        val flows =
-            listOf(x + 1 to y, x - 1 to y).map { it to (grid[it] ?: ".") }.filter { it.second == "|" }
-        // is there a block to the other side
-        when {
-            flows.size == 2 -> return false
-            flows.size == 1 -> {
-                var where = flows.single().first
-                val inc = when (where) {
-                    x + 1 to y -> 1
-                    x - 1 to y -> -1
-                    else -> 0
-                }
-                if (inc == 0) return true
-
-                while (grid[where] == "|") {
-                    where = where.first + inc to where.second
-                }
-
-                // if there is a path out then dont freeze
-                return grid[where] != null
-            }
-            else -> return true
-        }
     }
 
     private fun print() {
-        val points = grid
-        val minX = (points.filterValues { it == "|" || it == "~" }.keys.minBy { it.first }?.first ?: points.keys.minBy { it.first }?.first!!) - 2
-        val maxX = (points.filterValues { it == "|" || it == "~" }.keys.maxBy { it.first }?.first ?: points.keys.maxBy { it.first }?.first!!) + 2
-        val minY = (points.filterValues { it == "|" || it == "~" }.keys.minBy { it.second }?.second ?: minY) - 2
-        val maxY = (points.filterValues { it == "|" || it == "~" }.keys.maxBy { it.second }?.second ?: maxY) + 2
-
-        for (i in minX until 500) print(".")
-        print("+")
-        for (i in 500 + 1..maxX) print(".")
         println()
-
-
-        for (j in 1..maxY) {
-            for (i in minX..maxX) {
-                print(if (points.contains(i to j)) points[i to j] else ".")
+        for (y in 0 until grid.size) {
+            val row = grid[y]
+            for (x in 0 until row.size) {
+                val char = row[x]
+                print(char)
             }
-
-            println("$j".padStart(10))
+            println()
         }
-
-        println()
-        println()
     }
-}
 
-private fun <K, V> MutableMap<K, V>.blocked(key: K): Boolean {
-    val get = get(key)
-    return get == null || get == "|"
+    private fun adjContains(c: Char, count: Int, x: Int, y: Int): Boolean {
+        return listOf(
+            getOrNull(y - 1, x - 1),
+            getOrNull(y - 1, x),
+            getOrNull(y - 1, x + 1),
+            getOrNull(y, x - 1),
+            getOrNull(y, x + 1),
+            getOrNull(y + 1, x - 1),
+            getOrNull(y + 1, x),
+            getOrNull(y + 1, x + 1)
+        ).filter { it == c }
+            .size >= count
+    }
+
+    private fun getOrNull(y: Int, x: Int): Char? {
+        return try {
+            grid[y][x]
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            null
+        } catch (e: IndexOutOfBoundsException) {
+            null
+        }
+    }
 }
